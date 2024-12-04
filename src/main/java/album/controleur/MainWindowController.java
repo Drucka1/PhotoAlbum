42c67b8f25bcc4e518301a1e7ae79d78003695ca
PLISSON Clement <clement.plisson@telecomnancy.eu>
2024-12-04 11:45:09 +0100

    ajout bouton retour a acceuil

--- src/main/java/album/controleur/MainWindowController.java
+++ src/main/java/album/controleur/MainWindowController.java
@@ -1,8 +1,32 @@
 package album.controleur;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 public class MainWindowController {
 
-    public MainWindowController(){
+    @FXML
+    private Button acceuil;
 
+    public MainWindowController(){}
+
+    @FXML
+    public void gotoAcceuil() throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Accueil.fxml"));
+        BorderPane root = loader.load();
+
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) acceuil.getScene().getWindow();  
+        
+        stage.setScene(scene);
+        
     }
+
 }
