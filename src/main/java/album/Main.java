826c9a2add5b83da9770685bfbb3f8c415eb35a6
PLISSON Clement <clement.plisson@telecomnancy.eu>
2024-12-04 11:34:29 +0100

    ajout d'une page d'acceuil avec selection de repertoire

--- src/main/java/album/Main.java
+++ src/main/java/album/Main.java
@@ -2,50 +2,19 @@ package album;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-
-import album.controleur.BrowseAlbumController;
-import album.controleur.MainWindowController;
-import album.controleur.OverviewController;
-import album.controleur.RepositoryController;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Album album = albumTest();
-        FXMLLoader loader = new FXMLLoader();
-
-        URL fxmlURL = getClass().getResource("/view/MainWindow.fxml");
-        if (fxmlURL == null) {
-            System.err.println("Could not find .fxml");
-            System.exit(1);
-        }
-        
-        MainWindowController mw = new MainWindowController();
-        OverviewController oc = new OverviewController(album);
-        BrowseAlbumController ba = new BrowseAlbumController(album);
-        RepositoryController rc = new RepositoryController("/home/clem/TN/2A/PCD/lab2-e4206u/src/main/resources/images/");
-        ba.addObserver(oc);
-
-        loader.setControllerFactory(controllerClass -> {
-            if (controllerClass.equals(MainWindowController.class)) return mw;
-            else if (controllerClass.equals(OverviewController.class)) return oc;
-            else if (controllerClass.equals(BrowseAlbumController.class)) return ba;
-            else if (controllerClass.equals(RepositoryController.class)) return rc;
-            return null; 
-        });
-
-        
-        loader.setLocation(fxmlURL);
-        Parent root = loader.load();
-    
-        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Accueil.fxml"));
+        BorderPane root = loader.load();
 
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
@@ -58,16 +27,6 @@ public class Main extends Application {
         launch();
     }
 
-    private Album albumTest(){
-        Album album = new Album("My Album");
-        album.addPhoto(new Photo("Left Page 1", "/images/test0.jpeg")); 
-        album.addPhoto(new Photo("Right Page 1", "/images/test8.png"));
-        album.addPhoto(new Photo("Left Page 2", "/images/test2.jpg"));
-        album.addPhoto(new Photo("Right Page 2", "/images/test5.png"));
-        album.addPhoto(new Photo("Left Page 3", "/images/test7.png")); 
-        album.addPhoto(new Photo("Right Page 3", "/images/test9.png"));
-        return album;
-
-    }
+    
 
 }
---
+++ src/main/java/album/controleur/AccueilController.java
@@ -0,0 +1,89 @@
+package album.controleur;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import album.Album;
+import album.Photo;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+public class AccueilController {
+
+    @FXML   
+    private Button button;
+    @FXML
+    private Label label;
+
+
+    public AccueilController(){}
+
+    public void gotoCreation() throws IOException{
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        
+        // Définir un répertoire initial (facultatif)
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        
+        // Ouvrir la fenêtre de sélection du répertoire
+        Stage stage = (Stage) button.getScene().getWindow();  // Récupérer le Stage actuel
+        File selectedDirectory = directoryChooser.showDialog(stage);
+
+        // Vérifier si un répertoire a été sélectionné
+        if (selectedDirectory != null) {
+            stage.setScene(loadCreation(selectedDirectory.getAbsolutePath()));
+        } else {
+            label.setText("Aucun répertoire sélectionné");
+        }
+
+    }
+
+    private Scene loadCreation(String directoryPath) throws IOException {
+        Album album = albumTest();
+        FXMLLoader loader = new FXMLLoader();
+
+        URL fxmlURL = getClass().getResource("/view/MainWindow.fxml");
+        if (fxmlURL == null) {
+            System.err.println("Could not find .fxml");
+            System.exit(1);
+        }
+        
+        MainWindowController mw = new MainWindowController();
+        OverviewController oc = new OverviewController(album);
+        BrowseAlbumController ba = new BrowseAlbumController(album);
+        RepositoryController rc = new RepositoryController(directoryPath);
+        ba.addObserver(oc);
+
+        loader.setControllerFactory(controllerClass -> {
+            if (controllerClass.equals(MainWindowController.class)) return mw;
+            else if (controllerClass.equals(OverviewController.class)) return oc;
+            else if (controllerClass.equals(BrowseAlbumController.class)) return ba;
+            else if (controllerClass.equals(RepositoryController.class)) return rc;
+            return null; 
+        });
+
+        
+        loader.setLocation(fxmlURL);
+        Parent root = loader.load();
+
+        return new Scene(root);
+    }
+
+    private Album albumTest(){
+        Album album = new Album("My Album");
+        album.addPhoto(new Photo("Left Page 1", "/images/test0.jpeg")); 
+        album.addPhoto(new Photo("Right Page 1", "/images/test8.png"));
+        album.addPhoto(new Photo("Left Page 2", "/images/test2.jpg"));
+        album.addPhoto(new Photo("Right Page 2", "/images/test5.png"));
+        album.addPhoto(new Photo("Left Page 3", "/images/test7.png")); 
+        album.addPhoto(new Photo("Right Page 3", "/images/test9.png"));
+        return album;
+
+    }
+}
