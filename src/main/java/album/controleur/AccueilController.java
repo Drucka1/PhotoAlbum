053e5d243dd005900867c9c39f61adeb1c259b22
PLISSON Clement <clement.plisson@telecomnancy.eu>
2025-01-03 17:52:45 +0100

    choix de l'album parmi ceux enregistré ou creation d'un nouveau

--- src/main/java/album/controleur/AccueilController.java
+++ src/main/java/album/controleur/AccueilController.java
@@ -3,14 +3,19 @@ package album.controleur;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 
 import album.structure.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
@@ -18,29 +23,34 @@ public class AccueilController {
 
     @FXML private Button button;
     @FXML private Label label;
-
+    @FXML private ListView<String> albumListView;
 
     public AccueilController(){}
 
-    public void gotoCreation() throws IOException{
-        DirectoryChooser directoryChooser = new DirectoryChooser();
+    @FXML
+    public void initialize() {
 
-        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        ObservableList<String> albums = loadAlbums();
 
-        Stage stage = (Stage) button.getScene().getWindow();  
-        File selectedDirectory = directoryChooser.showDialog(stage);
-        
-        if (selectedDirectory != null) {
-            stage.setScene(loadCreation(selectedDirectory.getAbsolutePath()));
-        } else {
-            label.setText("Aucun répertoire sélectionné");
-        }
+        albumListView.getItems().clear();
+        albumListView.setItems(albums);
 
+        albumListView.setOnMouseClicked(event -> {
+            String selectedAlbum = albumListView.getSelectionModel().getSelectedItem();
+            if (selectedAlbum != null) {
+                try {
+                    gotoCreation(selectedAlbum);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
-    private Scene loadCreation(String directoryPath) throws IOException {
+    
+    private Scene loadCreation(Album album, String directoryPath) throws IOException {
         //Album album = albumTest();
-        Album album = AlbumDB.loadAlbum("My Album");
+        //Album album = AlbumDB.loadAlbum(albumName);
         FXMLLoader loader = new FXMLLoader();
 
         URL fxmlURL = getClass().getResource("/view/MainWindow.fxml");
@@ -70,15 +80,71 @@ public class AccueilController {
         return new Scene(root);
     }
 
-    private Album albumTest(){
-        Album album = new Album("My Album");
-        album.addPhoto(new Photo("Left Page 1", "/images/test0.jpeg")); 
-        album.addPhoto(new Photo("Right Page 1", "/images/test8.png"));
-        album.addPhoto(new Photo("Left Page 2", "/images/test2.jpg"));
-        album.addPhoto(new Photo("Right Page 2", "/images/test5.png"));
-        album.addPhoto(new Photo("Left Page 3", "/images/test7.png")); 
-        album.addPhoto(new Photo("Right Page 3", "/images/test9.png"));
-        return album;
+    
 
+    private ObservableList<String> loadAlbums() {
+        // Simuler le chargement des albums depuis un répertoire
+        File albumDir = new File("./src/main/resources/albums/");
+        File[] files = albumDir.listFiles(); 
+        System.out.println(files);
+
+        ObservableList<String> albumNames = FXCollections.observableArrayList();
+        if (files != null) {
+            for (File file : files) {
+                albumNames.add(file.getName()); 
+            }
+        }
+        return albumNames;
+    }
+    
+    private void gotoCreation(String albumName) throws IOException{
+        String path = askForPath();
+        Stage stage = (Stage) button.getScene().getWindow();  
+        if (path != null){
+            Album album = AlbumDB.loadAlbum(albumName);
+            stage.setScene(loadCreation(album,path));
+        }
+        else {
+            label.setText("Aucun répertoire sélectionné");
+        }
+
+    }
+
+
+    @FXML 
+    public void handleNewAlbum() throws IOException{
+        Stage stage = (Stage) button.getScene().getWindow();  
+
+        String name = askForAlbumName();
+        String path = askForPath();
+
+        if (name != null && path != null)  stage.setScene(loadCreation(new Album(name), path));
+    }
+
+    private String askForAlbumName(){
+
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Nom de l'album");
+        dialog.setHeaderText("Veuillez entrer le nom de l'album");
+        dialog.setContentText("Nom de l'album :");
+
+        
+        Optional<String> name = dialog.showAndWait();
+    
+        return name.orElse(null);
+    }
+    
+    private String askForPath(){
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+
+        Stage stage = (Stage) button.getScene().getWindow();  
+        File selectedDirectory = directoryChooser.showDialog(stage);
+        
+        if (selectedDirectory != null) {
+            return selectedDirectory.getAbsolutePath();
+        } 
+        return null;
     }
 }
